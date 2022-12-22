6592616bd89c42b6bbb8bcb5c9c63bdbf73a66e5
Joshua Albrecht <joshua.albrecht@mail.de>
2022-12-22 19:04:28 +0100

    fliyerss

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 function toggleNav() {
     document.querySelector(".menu").dataset.nav = document.querySelector(".menu").dataset.nav === "true" ? "false" : "true";
 }
+
+let nav = document.querySelector("nav").style;
+
+window.onscroll = () => {
+    if (this.oldScroll > this.scrollY) {
+        if (this.scrollY > 0) {
+            nav.backgroundColor = "f00";
+        } else {
+            nav.backgroundColor = "#0f0";
+        }
+    } else {
+        nav.backgroundColor = "00f";
+    }
+
+    this.oldScroll = this.scrollY;
+}
