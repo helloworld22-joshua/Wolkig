9197916bef150c0cce574d6e83eee7e284f0b31d
Joshua Albrecht <joshua.albrecht@mail.de>
2023-02-12 23:05:06 +0100

    Fixed nav

--- script.js
+++ script.js
@@ -9,15 +9,19 @@ window.onscroll = () => {
     if (this.oldScroll > this.scrollY) {
         if (this.scrollY > 0) {     // Scroll up
             nav.top = "0";
-            menu.opacity = "1";
             nav.backgroundColor = "var(--sec-color)";
+            menu.bottom = "-75vh";
+            menu.opacity = "1";
         } else {     // At top
             nav.background = "none";
         }
     } else {     // Scroll down
-        nav.top = "-100px";
-        menu.opacity = "0";
+        nav.top = "-90px";
+        menu.bottom = "calc(-75vh - 90px)";
+        if (!document.querySelector(".menu").dataset.nav) {
+            menu.opacity = "0";
+        }
     }
-
+    console.log(window.innerWidth);
     this.oldScroll = this.scrollY;
 }
