a7516e63fc89a61829127df556b580a38497daa6
Joshua Albrecht <joshua.albrecht@mail.de>
2023-02-18 20:14:49 +0100

    🥨

--- script.js
+++ script.js
@@ -1,27 +1,19 @@
 function toggleNav() {
-    document.querySelector(".menu").dataset.nav = document.querySelector(".menu").dataset.nav === "true" ? "false" : "true";
+    document.querySelector(".menu").dataset.menu = document.querySelector(".menu").dataset.menu === "true" ? "false" : "true";
 }
 
-let nav = document.querySelector("nav").style;
-let menu = document.querySelector("nav .menu").style;
+let nav = document.querySelector("nav").dataset;
 
 window.onscroll = () => {
-    if (this.oldScroll > this.scrollY) {
-        if (this.scrollY > 0) {     // Scroll up
-            nav.top = "0";
-            nav.backgroundColor = "var(--sec-color)";
-            menu.bottom = "-75vh";
-            menu.opacity = "1";
-        } else {     // At top
-            nav.background = "none";
-        }
+    if (this.oldScroll > this.scrollY) {    // Scroll up
+            if (this.scrollY == 0) {     // At top
+                nav.scroll = "top";
+            } else {
+                nav.scroll = "up";
+            }
     } else {     // Scroll down
-        nav.top = "-90px";
-        menu.bottom = "calc(-75vh - 90px)";
-        if (!document.querySelector(".menu").dataset.nav) {
-            menu.opacity = "0";
-        }
+        nav.scroll = "down";
     }
-    console.log(window.innerWidth);
+
     this.oldScroll = this.scrollY;
 }
