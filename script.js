873f42e97545e5c5e2e4ce850260d3c57430c2b0
Joshua Albrecht <joshua.albrecht@mail.de>
2023-05-29 22:16:39 +0200

    Fixed module and added nav feature

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-function toggleNav() {
-    document.querySelector(".menu").dataset.menu = document.querySelector(".menu").dataset.menu === "true" ? "false" : "true";
-}
+"use strict";
 
+// declare variables for better overview
+let menu = document.querySelector(".menu").dataset;
 let nav = document.querySelector("nav").dataset;
 
+// Set menu data true when false and vice versa
+function toggleNav() {
+    menu.menu = menu.menu === "true" ? "false" : "true";
+}
+
+// Show, hide, change color of nav depending on scroll position
 window.onscroll = () => {
-    if (this.oldScroll > this.scrollY) {    // Scroll up
+    if (this.lastScroll > this.scrollY) {    // Scroll up
             if (this.scrollY == 0) {     // At top
                 nav.scroll = "top";
             } else {
@@ -15,5 +21,10 @@ window.onscroll = () => {
         nav.scroll = "down";
     }
 
-    this.oldScroll = this.scrollY;
+    this.lastScroll = this.scrollY;
+}
+
+// If anything besides the menu is clicked, menu data will be set false
+window.onclick = (e) => {
+    if (!e.target.matches(".menu *")) menu.menu = "false";
 }
