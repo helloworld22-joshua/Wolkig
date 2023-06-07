7401930fe93be931c796f9f15db1f7560fca2354
Joshua Albrecht <joshua.albrecht@mail.de>
2023-06-07 20:13:03 +0200

    Test before production

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 "use strict";
 
 // declare variables for better overview
-let menu = document.querySelector(".menu").dataset;
-let nav = document.querySelector("nav").dataset;
+let menuData = document.querySelector(".menu").dataset;
+let navData = document.querySelector("nav").dataset;
 
-// Set menu data true when false and vice versa
+// Set menuData true when false and vice versa
 function toggleNav() {
-    menu.menu = menu.menu === "true" ? "false" : "true";
+    menuData.menu = menuData.menu === "true" ? "false" : "true";
 }
 
 // Show, hide, change color of nav depending on scroll position
-window.onscroll = () => {
+window.addEventListener("scroll", () => {
     if (this.lastScroll > this.scrollY) {    // Scroll up
-            if (this.scrollY == 0) {     // At top
-                nav.scroll = "top";
-            } else {
-                nav.scroll = "up";
-            }
+        if (this.scrollY === 0) {     // At top
+            navData.scroll = "top";
+        } else {
+            navData.scroll = "up";
+        }
     } else {     // Scroll down
-        nav.scroll = "down";
+        navData.scroll = "down";
     }
 
     this.lastScroll = this.scrollY;
-}
+});
 
-// If anything besides the menu is clicked, menu data will be set false
-window.onclick = (e) => {
-    if (!e.target.matches(".menu *")) menu.menu = "false";
-}
+// If anything besides the menu is clicked, menuData will be set false
+window.addEventListener("click", (e) => {
+    if (!e.target.matches(".menu *")) menuData.menu = "false";
+});
